fadb64e232074a72d17431250363188bc1f92f5c
Solaris <ovvo.system@gmail.com>
2023-07-23 08:35:42 -0300

    Add createGrid() function

---
+++ src/etch-a-sketch.js
@@ -0,0 +1,21 @@
+const gridContainer = document.querySelector(".grid");
+
+function createGrid(size) {
+    /* Creates grid of size x size, with a limit of size 100 */
+    if (size > 100) {
+        throw new Error("Parameter size exceeds max value of 100");
+    }
+
+    for (let i = 0; i < size; i++) { // For every row
+        const gridRow = document.createElement("div");
+        gridRow.setAttribute("class", "grid-row");
+
+        for (let j = 0; j < size; j++) { // For every column
+            const gridPixel = document.createElement("div");
+            gridPixel.setAttribute("class", "grid-pixel");
+            gridRow.appendChild(gridPixel);
+        }
+
+        gridContainer.appendChild(gridRow);
+    }
+}
